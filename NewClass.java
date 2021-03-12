38e0528b2eb32ed919ab73b04477cfe67df31d47
unknown <mphaphulimulamuleli@gmail.com>
2021-03-12 13:12:08 +0200

    mulamuleli

---
+++ NewClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author TC-13
+ */
+public class NewClass {
+    NewClass(){
+        System.out.println("mulamuleli");
+    }
+}
